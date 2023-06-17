d965809b59d3134a46df938503ad1dd045230f91
Andres Alonso <108694498+AXAStudio@users.noreply.github.com>
2023-06-16 21:02:59 -0400

    category id call done; added requests to requirements

--- utils/api_parser.py
+++ utils/api_parser.py
@@ -1,15 +1,32 @@
 # helper script to make footapi more readable
+import requests
 
-def general_request(api_link, key, details):
+
+HEADERS = {
+    "X-RapidAPI-Key": "0b59cdea82msh8c62aa043ee53dcp1d2033jsn82befb9a89bc",
+    "X-RapidAPI-Host": "footapi7.p.rapidapi.com"
+}
+
+
+def clean_input(inputstr):
+    return inputstr.lower().strip().replace(" ", "-")
+
+def general_request(details):
     #stuff to get api data
-    #country, league, year = details
-    #category_id = get_category_id(country)
+    details = map(clean_input, details)
+    country, league, year = details
+    category_id = get_category_id(country)
     #...
     #return (category_id, tournament_id, season_id)
-    pass
+    return details
 
 def get_category_id(country): #category = country (i.e. US)
-    pass
+    request_url = f"https://footapi7.p.rapidapi.com/api/tournament/categories"
+    response = requests.get(request_url, headers=HEADERS).json()
+    for row in response["categories"]:
+        if row["slug"] == country:
+            return row["id"]
+    return None
 
 def get_tournament_id(category_id, league): #tournament = league (i.e. MLS)
     pass
@@ -18,8 +35,6 @@ def get_season_id(tournament_id, year): #season = season (i.e. MLS 2023 season)
     pass
 
 if __name__ == "__main__":
-    api = ""
-    api_key = ""
-    details = input().replace(" ", "").split(",")
-    ids = general_request(api, api_key, details)
+    details = input("Details: ").replace(" ", "").split(",")
+    ids = general_request(details)
     print(ids)
