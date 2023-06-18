f97a4115eef06e52e3da299b3e539b84eb60cbf0
Andres Alonso <108694498+AXAStudio@users.noreply.github.com>
2023-06-18 09:37:24 -0400

    Started selection form

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 from utils.api_parser import FootAPIParser
 import streamlit as st
 import pandas as pd
+import os
 
 PARSER = FootAPIParser()
 
 st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":soccer:")
 st.title('Welcome to SOP Bot :gear:')
 st.caption("A Proprietary Soccer Outcome Prediction Algorithm By Alejandro and Andres Alonso")
-st.form()
-country = st.text_input('Country')
-next_input = st.submit()
 
-if valid_country(country):
-    st.text_input('League')
+#next_input = st.submit()
+def valid_country(country):
+    if country:
+        return True
 
+def valid_league(league):
+    if league:
+        return True
+
+def valid_team(team):
+    if team:
+        return True
+with st.form("Country"):
+    st.text_input('Country')
+    submitted = st.form_submit_button("To League")
+    if submitted:
+        st.text_input('League')
+        next = st.form_submit_button("To Team")
+        if next:
+            st.text_input('Team')
+
+        #next2 = st.form_submit_button("To Team")
+    # Every form must have a submit button.
+#    if next2:
+#        
+
+    
+    
+
+st.write("Outside the form")
--- utils/lists.py
+++ utils/lists.py
@@ -1,11 +1,19 @@
 from api_parser import FootAPIParser
+import requests
 
 class Scraper(FootAPIParser):
-    def get_country_list():
-        pass
+    def get_country_list(self):
+        country_list = []
+        request_url = "https://footapi7.p.rapidapi.com/api/tournament/categories"
+        response = requests.get(request_url, headers=self._headers).json()
+        with open("static/country_list.txt", "a") as f:
+            for row in response["categories"]:
+                f.write(row["slug"], "\n")
 
     def get_league_list(country_id):
         pass
 
     def get_team_list(league_id):
         pass
+Scraper = Scraper()
+Scraper.get_country_list()
