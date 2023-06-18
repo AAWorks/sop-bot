f0726344fee39305cdd899eb9a66da2823768b9e
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-06-18 14:39:37 -0400

    minor edit

--- main.py
+++ main.py
@@ -3,39 +3,17 @@ import streamlit as st
 import pandas as pd
 import os
 
-PARSER = FootAPIParser()
-
 st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":soccer:")
 st.title('Welcome to SOP Bot :gear:')
 st.caption("A Proprietary Soccer Outcome Prediction Algorithm By Alejandro and Andres Alonso")
 
-#next_input = st.submit()
-def valid_country(country):
-    if country:
-        return True
+with st.form("league"):
+    league = st.text_input("League")
+    userinput = st.form_submit_button("Continue")
+    if userinput:
+        home = st.text_input("Team 1")
+        away = st.text_input('Team 2')
+        predict = st.form_submit_button("Get Prediction")
 
-def valid_league(league):
-    if league:
-        return True
-
-def valid_team(team):
-    if team:
-        return True
-with st.form("Country"):
-    st.text_input('Country')
-    submitted = st.form_submit_button("To League")
-    if submitted:
-        st.text_input('League')
-        next = st.form_submit_button("To Team")
-        if next:
-            st.text_input('Team')
-
-        #next2 = st.form_submit_button("To Team")
-    # Every form must have a submit button.
-#    if next2:
-#        
-
-    
-    
-
-st.write("Outside the form")
+if predict:
+    st.write("working")
