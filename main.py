a4f33cb6b524c41f890a7c128fd65b35abd67249
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-06-18 14:46:02 -0400

    main edit

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ st.caption("A Proprietary Soccer Outcome Prediction Algorithm By Alejandro and A
 with st.form("league"):
     league = st.text_input("League")
     userinput = st.form_submit_button("Continue")
-    if userinput:
+
+if userinput:
+    with st.form("teams"):
         home = st.text_input("Team 1")
         away = st.text_input('Team 2')
         predict = st.form_submit_button("Get Prediction")
