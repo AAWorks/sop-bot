5fbb89ec82209f47f4828c7fb5df52ae414a21c0
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-06-19 18:48:47 -0400

    scraper scaffold

--- utils/scraper.py
+++ utils/scraper.py
@@ -9,28 +9,15 @@ class Scraper:
     def __init__(self):
         self._footapi = FootAPISearch()
     
-    def get_premier_league_ids(self): #gets the league ids for all the divisions
+    def get_mls_league_ids(self): #gets the league ids for all the mls divisions
         pass
 
-    def get_league_matches(self, league_id):
+    def get_mls_league_match_ids(self): #get all the mls leagues match ids
         pass
 
-    def get_match_data(self, match_id):
+    def get_league_matches(self, league_id): #skip this for now
         pass
 
-class Stats(FootAPIParser):
-    def get_country_list(self):
-        country_list = []
-        request_url = "https://footapi7.p.rapidapi.com/api/tournament/categories"
-        response = requests.get(request_url, headers=self._headers).json()
-        with open("static/country_list.txt", "a") as f:
-            for row in response["categories"]:
-                f.write(row["slug"], "\n")
-
-    def get_league_list(country_id):
+    def get_match_data(self, match_id): #get all the relevant stats from a match and put them into a list
         pass
 
-    def get_team_list(league_id):
-        pass
-Scraper = Scraper()
-Scraper.get_country_list()
