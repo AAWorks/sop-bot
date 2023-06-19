25100f87642014d298da4f77956cb86249092795
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-06-19 18:10:28 -0400

    ui works

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import os
 st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":soccer:")
 st.title('Welcome to SOP Bot :gear:')
 st.caption("A Proprietary Soccer Outcome Prediction Algorithm By Alejandro and Andres Alonso")
+predict, userinput = False, False
 
 with st.form("league"):
     league = st.text_input("League")
     userinput = st.form_submit_button("Continue")
 
-if userinput:
+if league:
     with st.form("teams"):
         home = st.text_input("Team 1")
         away = st.text_input('Team 2')
         predict = st.form_submit_button("Get Prediction")
 
-    if predict:
-        st.write("working")
+    if home and away:
+        st.success("working")
