85ecb2975c92112145f46965bc62b5647a0d4d41
Andres Alonso <108694498+AXAStudio@users.noreply.github.com>
2023-06-20 08:51:18 -0400

    get_league_match_ids working

--- utils/scraper.py
+++ utils/scraper.py
@@ -9,15 +9,24 @@ class Scraper:
     def __init__(self):
         self._footapi = FootAPISearch()
     
-    def get_mls_league_ids(self): #gets the league ids for all the mls divisions
+    def get_league_ids(self): #gets the league ids for all the mls divisions
         pass
 
-    def get_mls_league_match_ids(self): #get all the mls leagues match ids
-        pass
-
+    def get_league_match_ids(self, league_id): #get all the mls leagues match ids
+        ids = []
+        url = f"https://footapi7.p.rapidapi.com/api/tournament/242/season/{league_id}/matches/last/0"
+        response = requests.get(url, headers=self._footapi._headers).json()
+        for row in response["events"]:
+            ids.append(row["id"])
+        return ids
+        
     def get_league_matches(self, league_id): #skip this for now
         pass
 
     def get_match_data(self, match_id): #get all the relevant stats from a match and put them into a list
         pass
 
+
+par = Scraper()
+ids = par.get_league_match_ids(47955)
+print(ids)
