89882e514777b567fa78b499833fbbf1d6d341a8
Andres Alonso <108694498+AXAStudio@users.noreply.github.com>
2023-06-20 20:49:49 -0400

    Added more extensive match details

--- utils/scraper.py
+++ utils/scraper.py
@@ -8,9 +8,6 @@ class Parser:
 class Scraper:
     def __init__(self):
         self._footapi = FootAPISearch()
-    
-    def get_league_ids(self): #gets the league ids for all the mls divisions
-        pass
 
     def get_league_match_ids(self, league_id): #get all the mls leagues match ids
         ids = []
@@ -24,9 +21,21 @@ class Scraper:
         pass
 
     def get_match_data(self, match_id): #get all the relevant stats from a match and put them into a list
-        pass
+        home_team = []
+        away_team = []
+        match_data = []
+        url = f"https://footapi7.p.rapidapi.com/api/match/{match_id}"
+        response = requests.get(url, headers=self._footapi._headers).json()
+        away_team.append(response["event"]["awayTeam"]["name"])
+        away_team.append(response["event"]["awayScore"]["current"])
+        home_team.append(response["event"]["homeTeam"]["name"])
+        home_team.append(response["event"]["homeScore"]["current"])
+        match_data.append(response["event"]["tournament"]["uniqueTournament"]["name"])
+        match_data.append(response["event"]["venue"]["stadium"]["name"])
+        match_data.append(response["event"]["season"]["year"])
+        return home_team, away_team, match_data
 
 
 par = Scraper()
-ids = par.get_league_match_ids(47955)
-print(ids)
+home_stats, away_stats, match_stats = par.get_match_data(10952277)
+print(f"{home_stats[0]}: {home_stats[1]}, {away_stats[0]}: {away_stats[1]} | Tournament: {match_stats[0]} | Venue: {match_stats[1]} | Year: {match_stats[2]}")
