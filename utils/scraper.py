42e645b956ef800b768b823e149a3b61cc215049
Andres Alonso <108694498+AXAStudio@users.noreply.github.com>
2023-06-22 21:03:13 -0400

    Fixed red card issue

--- utils/scraper.py
+++ utils/scraper.py
@@ -91,7 +91,7 @@ class Parser:
 
     def _add_row(self, values):
         cursor = self._db.cursor()
-        cursor.execute("INSERT INTO mls(match_id, epoch_date, home_team, home_score, away_team, away_score, stadium_name, ref_toughness, home_possession, away_possession, home_totshots, away_totshots, home_shotsontarget, away_shotsontarget, home_shotsofftarget, away_shotsofftarget, home_blockedshots, away_blockedshots, home_cornerkicks, away_cornerkicks, home_offsides, away_offsides, home_fouls, away_fouls, home_yellowcards, away_yellowcards, home_redcards, away_redcards, home_bigchances, away_bigchances, home_bigchancesmissed, away_bigchancesmissed, home_hitwoodwork, away_hitwoodwork, home_counterattacks, away_counterattacks, home_counterattackshotsmissed, away_counterattackshotsmissed, home_shotsinsidebox, away_shotsinsidebox, home_shotsoutsidebox, away_shotsoutsidebox, home_goalkeepersaves, away_goalkeepersaves, home_goalsprevented, away_goalsprevented, home_passes, away_passes, home_accuratepasses, away_accuratepasses, home_longballs, away_longballs, home_crosses, away_crosses, home_dribbles, away_dribbles, home_possessionlost, away_possessionlost, home_duelswon, away_duelswon, home_aerialswon, away_aerialswon, home_tackles, away_tackles, home_interceptions, away_interceptions, home_clearances, away_clearances, home_formation, away_formation", *values)
+        cursor.execute("""INSERT INTO mls(match_id, epoch_date, home_team, home_score, away_team, away_score, stadium_name, ref_toughness, home_possession, away_possession, home_totshots, away_totshots, home_shotsontarget, away_shotsontarget, home_shotsofftarget, away_shotsofftarget, home_blockedshots, away_blockedshots, home_cornerkicks, away_cornerkicks, home_offsides, away_offsides, home_fouls, away_fouls, home_yellowcards, away_yellowcards, home_redcards, away_redcards, home_bigchances, away_bigchances, home_bigchancesmissed, away_bigchancesmissed, home_hitwoodwork, away_hitwoodwork, home_counterattacks, away_counterattacks, home_counterattackshotsmissed, away_counterattackshotsmissed, home_shotsinsidebox, away_shotsinsidebox, home_shotsoutsidebox, away_shotsoutsidebox, home_goalkeepersaves, away_goalkeepersaves, home_goalsprevented, away_goalsprevented, home_passes, away_passes, home_accuratepasses, away_accuratepasses, home_longballs, away_longballs, home_crosses, away_crosses, home_dribbles, away_dribbles, home_possessionlost, away_possessionlost, home_duelswon, away_duelswon, home_aerialswon, away_aerialswon, home_tackles, away_tackles, home_interceptions, away_interceptions, home_clearances, away_clearances, home_formation, away_formation) VALUES(?, ?, ?)""", values)
         self._db.commit()
 
 class Scraper:
@@ -127,9 +127,19 @@ class Scraper:
         for row in response[2]["statisticsItems"]:
             match_data.append(int(row["home"]))
             match_data.append(int(row["away"]))
+        i = 0
         for row in response[3]["statisticsItems"]:
+            if i == 4:
+                break
             match_data.append(int(row["home"]))
             match_data.append(int(row["away"]))
+            i += 1
+        try:
+            match_data.append(int(response[3]["statisticsItems"][4]["home"]))
+            match_data.append(int(response[3]["statisticsItems"][4]["away"]))
+        except:
+            match_data.append(0)
+            match_data.append(0)
         for row in response[4]["statisticsItems"]:
             match_data.append(float(row["home"]))
             match_data.append(float(row["away"]))
@@ -154,12 +164,12 @@ class Scraper:
         response = requests.get(url, headers=self._footapi._headers).json()
         match_data.append(response["home"]["formation"])
         match_data.append(response["away"]["formation"])
-        return match_data
+        return tuple(match_data)
 
 
 scr = Scraper()
 par = Parser()
 par._create_table()
-par._add_row(scr.get_match_data(10408291))
+print(scr.get_match_data(10408291))
 #par._close_db()
 #print(f"{home_stats[0]}: {home_stats[1]}, {away_stats[0]}: {away_stats[1]} | Tournament: {match_stats[0]} | Stadium: {match_stats[1]} | Year: {match_stats[2]}")
