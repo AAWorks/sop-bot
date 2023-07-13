ebad297dd66df88ac0b448c2e642b5ccf769a65e
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-12 22:35:23 -0400

    the dnn algo training init done (loss function not great due to a binary crossentropy loss func being used on non binary outputs (0: loss, 1: win, 2: tie)

--- tfx_algo.py
+++ tfx_algo.py
@@ -1,17 +1,49 @@
-import tensorflow as tf
 import numpy as np
-
-from utils.parse import Dataset
-
-TRAINING_SET_FRACTION = 0.90
+import tensorflow as tf
+import tensorflow.keras as keras # pylance raises <Import "tensorflow.keras" could not be resolved> - Ignore
 
 
-def main(argv):
-    raw = Dataset("mls")
-    data = raw.aggregate_data(10)
-    raw.close_db()
+class DNNModel:
+    def __init__(self, records):
+        training_set_frac = 0.90
 
-    train_results_len = int(TRAINING_SET_FRACTION * len(data))
-    train_results = data[:train_results_len]
-    test_results = data[train_results_len:]
+        features = list(records.columns)
+        features.remove("result")
+
+        train_len = int(training_set_frac * records.shape[0])
+        train = records.head(train_len)
+        test = records.tail(records.shape[0] - train_len)
+
+        self._train = {
+            'data': np.array(train[features]), 
+            'labels': np.array(train["result"])
+        }
+
+        self._test = {
+            'data': np.array(test[features]), 
+            'labels': np.array(test["result"])
+        }
+
+        self._init_layer = records.shape[1] - 1
+
+        self._model = tf.keras.Sequential()
+
+        self._hist_obj = None
+    
+    def build(self):
+        self._model.add(tf.keras.layers.Dense(units=self._init_layer, activation='relu', input_shape=[self._init_layer,]))
+        self._model.add(tf.keras.layers.Dense(units=self._init_layer * 3, activation='relu'))
+        self._model.add(tf.keras.layers.Dense(units=self._init_layer * 4, activation='relu'))
+        self._model.add(tf.keras.layers.Dense(units=self._init_layer * 3, activation='relu'))
+        self._model.add(tf.keras.layers.Dense(units=1, activation='relu'))
+        opt = tf.keras.optimizers.Adam(learning_rate=1e-7)
+        self._model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['acc'])
+
+    def train(self):
+        self._hist_obj = self._model.fit(self._train['data'], self._train['labels'], verbose=False, epochs=500)
+    
+    def train_analytics(self):
+        if self._hist_obj:
+            return self._hist_obj.history
+        return None
 
