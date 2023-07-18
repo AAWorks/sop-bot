8986d807ed0f77b85c070f22e319e88fb8a38fed
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-18 01:59:18 -0400

    meh

--- tfx_algo.py
+++ tfx_algo.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 class DNNModel:
     def __init__(self, records):
-        training_set_frac = 0.90
+        training_set_frac = 0.85
 
         self._features = list(records.columns)
         self._features.remove("result")
@@ -35,17 +35,16 @@ class DNNModel:
     
     def build(self):
         #self._model.add(tf.keras.layers.Dropout(0.2, input_shape=[self._init_layer,]))
-        self._model.add(tf.keras.layers.Dense(units=self._init_layer, activation='relu', input_shape=[self._init_layer,]))
-        self._model.add(tf.keras.layers.Dropout(0.5))
-        self._model.add(tf.keras.layers.Dense(units=self._init_layer * 2 // 3, activation='relu'))
-        #self._model.add(tf.keras.layers.Dropout(0.5))
-        #self._model.add(tf.keras.layers.Dense(units=23, activation='relu'))
+        self._model.add(tf.keras.layers.Dense(units=self._init_layer, activation='sigmoid', input_shape=[self._init_layer,]))
+        # self._model.add(tf.keras.layers.Dropout(0.8))
+        self._model.add(tf.keras.layers.Dense(units=21, activation='sigmoid'))
+        # self._model.add(tf.keras.layers.Dense(units=8, activation='relu'))
         self._model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))#, activation='relu'))
         opt = tf.keras.optimizers.Adam(learning_rate=0.001) #learning_rate=1e-7
         self._model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=False), optimizer=opt, metrics=['accuracy']) # from_logits?
 
     def train(self):
-        self._hist_obj = self._model.fit(self._train["data"], self._train['labels'], validation_split=0.15, verbose=1, epochs=500, batch_size=64, shuffle=True)
+        self._hist_obj = self._model.fit(self._train["data"], self._train['labels'], validation_split=0.20, verbose=1, epochs=200, batch_size=32)#, callbacks=[earlystopping])
         # self._test["data"], self._test['labels']
     
     def train_analytics(self):
@@ -54,12 +53,12 @@ class DNNModel:
         return None
     
     def evaluate(self):
-        test = self._get_test_data(0)
+        test = self._get_test_data()
         eval = self._model.evaluate(test["data"], test['labels'], verbose=1, return_dict=True, batch_size=32)     
         return eval
     
     def get_test_predictions(self):
-        test = self._get_test_data(0)
+        test = self._get_test_data()
         eval = self._model.predict(test["data"], verbose=1, batch_size=32)
         
         return eval
