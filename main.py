190844854b5597be08c8c21d8e090b4434431891
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-27 11:00:25 -0400

    Metric adjustments

--- main.py
+++ main.py
@@ -97,18 +97,8 @@ with pred:
 with tfkeras:
     st.info("Tensorflow/Keras Deep Neural Network Model Summary & Evaluation Metrics")
     st.code(model.summary(), language='python')
-    # st.divider()
     history = model.train_analytics()
     trainstat = model.evaluate_train_on_confidence()
-    # col1, col2, col3, col4 = st.columns(4)
-    # val_acc = round(model.evaluate_on_confidence() * 100, 2)
-    # loss_acc = round(model.evaluate_on_confidence(0) * 100, 2)
-    # win_acc = round(model.evaluate_on_confidence(1) * 100, 2)
-    train_acc = round(model.evaluate_train_on_confidence() * 100, 2)
-    # col1.metric(label="Testing Accuracy", value=f"{val_acc}%", delta=f"{round(val_acc - 50, 2)}%")
-    # col2.metric(label="Testing Accuracy (Wins)", value=f"{win_acc}%", delta=f"{round(win_acc - 50, 2)}%")
-    # col3.metric(label="Testing Accuracy (Losses)", value=f"{loss_acc}%", delta=f"{round(loss_acc - 50, 2)}%")
-    # col4.metric(label="Training Accuracy", value=f"{train_acc}%", delta=f"{round(train_acc - 50, 2)}%")
     style_metric_cards()
 
     st.divider()
@@ -117,7 +107,7 @@ with tfkeras:
         data = pd.DataFrame()
         data['val_loss'] = history['val_loss']
         data['epoch'] = list(range(data.shape[0]))
-        st.metric("Validation Loss", str(data["val_loss"].min())[:6])
+        st.metric("Test Loss", str(data["val_loss"].min())[:6])
         sparkline_chart(
             data=data,
             x="epoch",
@@ -127,9 +117,9 @@ with tfkeras:
         )
     with testacc:
         data = pd.DataFrame()
-        data['val_accuracy'] = history['val_precision']
+        data['val_accuracy'] = history['val_accuracy']
         data['epoch'] = list(range(data.shape[0]))
-        st.metric("Validation Precision", str(data["val_accuracy"].max() * 100)[:5] + "%", delta=f"{round(data['val_accuracy'].max() * 100 - 50, 2)}%")
+        st.metric("Test Accuracy", str(data["val_accuracy"].max() * 100)[:5] + "%", delta=f"{round(data['val_accuracy'].max() * 100 - 50, 2)}%")
         sparkline_chart(
             data=data,
             x="epoch",
@@ -151,9 +141,9 @@ with tfkeras:
         )
     with trainacc:
         data = pd.DataFrame()
-        data['accuracy'] = history['precision']
+        data['accuracy'] = history['accuracy']
         data['epoch'] = list(range(data.shape[0]))
-        st.metric("Training Precision", str(train_acc) + "%", delta=f"{round(data['accuracy'].max() * 100 - 50, 2)}%")
+        st.metric("Training Accuracy", str(train_acc) + "%", delta=f"{round(data['accuracy'].max() * 100 - 50, 2)}%")
         sparkline_chart(
             data=data,
             x="epoch",
@@ -161,10 +151,14 @@ with tfkeras:
             height=80,
             autoscale_y=True,
         )
-    # st.line_chart(history['val_loss']) #NEEDS BETTER IMPLEMENTATION
-    # st.line_chart(history['val_accuracy'])
-    # st.line_chart(history['loss'])
-    # st.line_chart(history['accuracy'])
+    st.divider()
+    other_metrics = [history[metric].max() for metric in ("val_precision", "val_recall", "precision", "recall")]
+    test_p, test_r, train_p, train_r = other_metrics
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric(label="Test Precision", value=f"{test_p}%", delta=f"{round(test_p - 50, 2)}%")
+    col2.metric(label="Test Recall", value=f"{test_r}%", delta=f"{round(test_r - 50, 2)}%")
+    col3.metric(label="Train Precision", value=f"{train_p}%", delta=f"{round(train_p - 50, 2)}%")
+    col4.metric(label="Train Recall", value=f"{train_r}%", delta=f"{round(train_r - 50, 2)}%")
     st.divider()
     predictions = model.get_test_predictions() * 100
     prediction_df = pd.DataFrame()
